a5052074052977b7d61d264cda95b3401ae7d618
not-varram <varshithsatti18@gmail.com>
2025-02-17 13:24:30 -0800

    fix(cors): allow cross-site cros for devlopment (will remove later)

--- server.js
+++ server.js
@@ -5,9 +5,20 @@ const { connectDB } = require('./config/db');
 const beaconRoutes = require('./routes/beacons');
 
 dotenv.config();
+
+// Configure CORS options
+const corsOptions = {
+    origin: '*', 
+    methods: ['GET', 'POST', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    exposedHeaders: ['Content-Range', 'X-Content-Range'], 
+    maxAge: 600, 
+    optionsSuccessStatus: 204
+};
+
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Connect to MongoDB
